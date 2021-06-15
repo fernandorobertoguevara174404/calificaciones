4611a1a8f912720932e29e8fdef6f9156fed025b
fernandorobertoguevara174404 <a174404@une.edu.mx>
2021-06-14 20:59:38 -0500

    Heroku end

--- app/models.py
+++ app/models.py
@@ -4,11 +4,14 @@ from flask_login import UserMixin
 from app import login
 
 class User(UserMixin, db.Model):
+    __tablename__="users"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), index=True, unique=True)
     name = db.Column(db.String(120))
     apellido_paterno = db.Column(db.String(120), index=True)
     apellido_materno = db.Column(db.String(120))
+    profesor = db.Column(db.Boolean, default=False)
+    admin = db.Column(db.Boolean, default=False)
     # Añadir telefono
     matricula=db.Column(db.String(10), index=True, unique=True)
     password_hash=db.Column(db.String(128))
@@ -20,44 +23,47 @@ class User(UserMixin, db.Model):
     def check_password(self,password):
         return check_password_hash(self.password_hash, password)
 
+    def create_admin(self,password):
+        self.email="admin@admin.com"
+        self.profesor=True
+        self.admin=True
+        self.set_password(password)
+
     def __repr__(self):
         return "Usuario: {}, email: {}".format(self.matricula, self.email)
 
 usuario_curso = db.Table("usuario_curso",
-    db.Column("id_user", db.Integer, db.ForeignKey("user.id"), primary_key=True),
-    db.Column("id_curso", db.Integer, db.ForeignKey("curso.id"), primary_key=True)
+    db.Column("id_user", db.Integer, db.ForeignKey("users.id"), primary_key=True),
+    db.Column("id_curso", db.Integer, db.ForeignKey("cursos.id"), primary_key=True)
     )
 
-# usuario_curso = db.Table("calificacion",
-#     db.Column("id", db.Integer, db.ForeignKey("user.id"), primary_key=True),
-#     db.Column("id_curso", db.Integer, db.ForeignKey("curso.id"), primary_key=True)
-#     )
-
 class Curso(db.Model):
+    __tablename__="cursos"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
-    id_profesor = db.Column(db.Integer, db.ForeignKey("user.id"))
+    id_profesor = db.Column(db.Integer, db.ForeignKey("users.id"))
     alumnos = db.relationship("User", secondary=usuario_curso, lazy="subquery", 
     backref=db.backref("cursos_de_alumno", lazy=True))
 
 
 class Tarea(db.Model):
+    __tablename__="tareas"
     id = db.Column(db.Integer, primary_key=True)
-    id_curso = db.Column(db.Integer, db.ForeignKey("curso.id"))
+    id_curso = db.Column(db.Integer, db.ForeignKey("cursos.id"))
     titulo = db.Column(db.String(150))
     fecha_de_creacion = db.Column(db.DateTime)
     fecha_de_entrega = db.Column(db.DateTime)
     descripcion = db.Column(db.String(1500))
     puntaje = db.Column(db.Integer)
+    calificaciones = db.relationship('Calificaciones', backref='tarea', lazy=True)
 
 class Calificaciones(db.Model):
+    __tablename__="calificaciones"
     id = db.Column(db.Integer, primary_key=True)
-    id_tarea = db.Column(db.Integer, db.ForeignKey("tarea.id"))
-    id_alumno = db.Column(db.Integer, db.ForeignKey("user.id"))
+    # id_curso = db.Column(db.Integer, db.ForeignKey("curso.id"))
+    id_tarea = db.Column(db.Integer, db.ForeignKey("tareas.id"))
+    id_alumno = db.Column(db.Integer, db.ForeignKey("users.id"))
     calificacion = db.Column(db.Integer)
-
-
-
 
 @login.user_loader
 def load_user(id):
--- config.py
+++ config.py
@@ -3,5 +3,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY=os.getenv("SECRET_KEY") or "faiosdfjasdf"
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    if os.getenv("DATABASE_URL"):
+        SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL").replace("postgres", "postgresql")
+    else:
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    print(SQLALCHEMY_DATABASE_URI)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    if os.getenv("ADMIN_PASS"):
+        ADMIN_PASS = os.getenv("ADMIN_PASS")
